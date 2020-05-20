326a9c783a2231f05ed14a9f80dc50668411741c
nirbhay1997 <nirbhay@msystechnologies.com>
2020-05-20 14:48:01 +0530

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# testinf_stove
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,19 @@
+name 'testinf_stove'
+maintainer 'Nirbhay'
+maintainer_email 'nirbhay@msystechnologies.com'
+license 'Apache-2.0'
+description 'Installs/Configures testinf_stove'
+version '0.1.0'
+chef_version '>= 14.0'
+
+# The `issues_url` points to the location where issues for this cookbook are
+# tracked.  A `View Issues` link will be displayed on this cookbook's page when
+# uploaded to a Supermarket.
+#
+# issues_url 'https://github.com/<insert_org_here>/testinf_stove/issues'
+
+# The `source_url` points to the development repository for this cookbook.  A
+# `View Source` link will be displayed on this cookbook's page when uploaded to
+# a Supermarket.
+#
+# source_url 'https://github.com/<insert_org_here>/testinf_stove'
